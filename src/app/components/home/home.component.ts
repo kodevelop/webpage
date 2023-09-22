a47d02a614047a8d5940973a87b45c5947cff9c0
Jens Malfait <jens.malfait@trustteam.be>
2023-09-22 17:03:12 +0200

    styled the home and services page and added hero image to home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,6 +9,15 @@ export class HomeComponent implements OnInit {
   themeToggle: HTMLInputElement | undefined;
 
   ngOnInit(): void {
+    const heroImage = document.getElementById('hero-image') as HTMLDivElement;
+
     this.themeToggle = document.getElementById('theme-toggle') as HTMLInputElement;
+    this.themeToggle.addEventListener('change', () => {
+      if (this.themeToggle?.checked) {
+        heroImage.style.backgroundImage = "url(../../../assets/img/hero-dark.png)";
+      } else {
+        heroImage.style.backgroundImage = "url(../../../assets/img/hero-light.png)";
+      }
+    });
   }
 }
