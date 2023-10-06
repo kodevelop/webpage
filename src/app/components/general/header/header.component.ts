22145d4504955095b5895a95af68b78c4a8ac95d
Jens Malfait <jens.malfait@trustteam.be>
2023-10-06 17:02:21 +0200

    Glued navigation header to top of the page and added transition on scrolling

--- src/app/components/general/header/header.component.ts
+++ src/app/components/general/header/header.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-
 export class HeaderComponent implements OnInit {
   themeToggle: HTMLInputElement | undefined;
+  isScrolled = false;
 
   ngOnInit(): void {
-    this.themeToggle = document.getElementById('theme-toggle') as HTMLInputElement;
+    this.themeToggle = document.getElementById(
+      'theme-toggle'
+    ) as HTMLInputElement;
     this.themeToggle.addEventListener('change', () => {
       if (this.themeToggle?.checked) {
         document.documentElement.classList.add('dark');
@@ -18,5 +20,19 @@ export class HeaderComponent implements OnInit {
         document.documentElement.classList.remove('dark');
       }
     });
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    const offset =
+      window.scrollY ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    if (offset > 0 && !this.isScrolled) {
+      this.isScrolled = true;
+    } else if (offset === 0 && this.isScrolled) {
+      this.isScrolled = false;
+    }
   }
 }
