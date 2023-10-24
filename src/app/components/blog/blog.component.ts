3d000688050737426fa9bc91357b7416bb177d39
Jens Malfait <jens.malfait@trustteam.be>
2023-10-24 14:14:53 +0200

    Added markdown style to match theme of site

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class BlogComponent {
   onError($event: Error | string) {
