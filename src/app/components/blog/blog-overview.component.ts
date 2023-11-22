253c35b0bc1155bca929d59c0df73264405d0998
Jens Malfait <jens.malfait@trustteam.be>
2023-11-22 22:09:57 +0100

    No initial blog selected Fixes #1

--- src/app/components/blog/blog-overview.component.ts
+++ src/app/components/blog/blog-overview.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { BlogService } from './services/blog.service';
-import { BehaviorSubject, Observable, map, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable, map, startWith, switchMap, take, tap } from 'rxjs';
 import { Blog } from './models/blog.models';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-overview',
   templateUrl: './blog-overview.component.html'
 })
 export class BlogOverviewComponent {  
-  private refresh$ = new BehaviorSubject<string>('');
+  private refresh$ = new BehaviorSubject<string>((''));
   readonly dataSource: Observable<Blog[]>;
   
-  constructor(private readonly blogService: BlogService) {
+  constructor(private readonly blogService: BlogService, private readonly router: Router, private readonly route: ActivatedRoute,) {
     this.dataSource = this.refresh$.pipe(
+      startWith(''), // Provide your initial filter value here
       switchMap((filterValue: string) =>
         this.blogService.getBlogList().pipe(
           map((blogs) => {
@@ -20,7 +22,10 @@ export class BlogOverviewComponent {
             return blogs;
           })
         )
-      )
+      ),
+      take(1),
+      tap(x => console.log('+++', x)),
+      tap(x => this.router.navigate([x[0].slug], {relativeTo: this.route}))
     );
   }
 
@@ -29,7 +34,15 @@ export class BlogOverviewComponent {
     this.refresh$.next(f);
   }
 
+  onBlogChanged(slug: string) {
+    console.log(slug);
+  }
+
   private refresh() {
     this.refresh$.next('');
+  }
+
+  private get currentBlogId(): number {
+    return 0;
   }
 }
